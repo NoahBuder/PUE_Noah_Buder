d67513ddee1b72094f3478e90ee635166c279a69
NoahBuder <bn7246@mci4me.at>
2025-04-24 17:59:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PUE_Noah_Buder
---
+++ src/sort.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def bubble_sort(the_array_to sort):
+
+    temp_array = the_array_to_sort.copy()
+    while sorted(test_array) != temp_array:
+        temp_array.random.shuffle 
+        
+
+    return[]
+
+
+if__name_=="__main__":
+    test_array = np.array([5, 2, 9, 1, 5, 6])
+    print(bubble_sort(test_array))
